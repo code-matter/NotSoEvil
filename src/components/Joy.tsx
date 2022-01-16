3f167687c49a9136de5fa461234eb44dd4d59ced
Alex Caissy <alex.dev.prog@gmail.com>
2022-01-16 18:08:28 -0500

    [FEATURE] - added JOY component

--- src/components/Joy.tsx
+++ src/components/Joy.tsx
@@ -7,7 +7,18 @@ interface Props {
 const Joy = (props: Props) => {
   return (
     <div className='joy container'>
-
+      <h2>PROPAGER LA JOIE, <br />
+        UNE IMAGE À LA FOIS.</h2>
+      <div className='paragraph'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec pharetra ut quam a interdum. Proin elementum feugiat quam
+        </p>
+        <p>
+          commodo orci ac ex vestibulum maximus. Etiam dapibus interdum velit,
+          eget vulputate quam efficitur vitae.
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -27,54 +27,58 @@ const HomePage: React.FC = () => {
 
   return (
     <div className='homepage container'>
-      <EvilHeader />
-      <div className='text'>
-        <p>TATTOOS & {isMobile && <br />} PLENTY MORE</p>
-        <h3 >D’LA <span className='teal'>COULEUR</span> PI BEN <br />
-          D’LA <span className='mauve'>BONNE HUMEUR</span></h3>
-      </div>
-      <div className='homepage-links' >
-        <Link className={`${hovered.id === 'rdv' ? 'hovered' : ''} disabled`}
-          style={{
-            backgroundColor: hovered.id === 'rdv' ? hovered.color : '',
-            outline: hovered.id === 'rdv' ? `1px solid ${hovered.color}` : ''
-          }}
-          id='rdv'
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          to="/form">RENDEZ-VOUS</Link>
-        <Link className={`${hovered.id === 'flash' ? 'hovered' : ''} disabled`}
-          style={{
-            backgroundColor: hovered.id === 'flash' ? hovered.color : '',
-            outline: hovered.id === 'flash' ? `1px solid ${hovered.color}` : ''
-          }}
-          id='flash'
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          to="/flash">FLASH</Link>
-        <Link className={`${hovered.id === 'shop' ? 'hovered' : ''} disabled`}
-          style={{
-            backgroundColor: hovered.id === 'shop' ? hovered.color : '',
-            outline: hovered.id === 'shop' ? `1px solid ${hovered.color}` : ''
-          }}
-          id='shop'
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          to="/shop">SHOP</Link>
-        <Link className={`${hovered.id === 'plus' ? 'hovered' : ''} disabled`}
-          style={{
-            backgroundColor: hovered.id === 'plus' ? hovered.color : '',
-            outline: hovered.id === 'plus' ? `1px solid ${hovered.color}` : ''
-          }}
-          id='plus'
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          to="/plus">PLUS PLUS PLUS</Link>
-      </div>
+      <section className='top'>
+        <EvilHeader />
+        <div className='text'>
+          <p>TATTOOS & {isMobile && <br />} PLENTY MORE</p>
+          <h3 >D’LA <span className='teal'>COULEUR</span> PI BEN <br />
+            D’LA <span className='mauve'>BONNE HUMEUR</span></h3>
+        </div>
+        <div className='homepage-links' >
+          <Link className={`${hovered.id === 'rdv' ? 'hovered' : ''} disabled`}
+            style={{
+              backgroundColor: hovered.id === 'rdv' ? hovered.color : '',
+              outline: hovered.id === 'rdv' ? `1px solid ${hovered.color}` : ''
+            }}
+            id='rdv'
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            to="/form">RENDEZ-VOUS</Link>
+          <Link className={`${hovered.id === 'flash' ? 'hovered' : ''} disabled`}
+            style={{
+              backgroundColor: hovered.id === 'flash' ? hovered.color : '',
+              outline: hovered.id === 'flash' ? `1px solid ${hovered.color}` : ''
+            }}
+            id='flash'
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            to="/flash">FLASH</Link>
+          <Link className={`${hovered.id === 'shop' ? 'hovered' : ''} disabled`}
+            style={{
+              backgroundColor: hovered.id === 'shop' ? hovered.color : '',
+              outline: hovered.id === 'shop' ? `1px solid ${hovered.color}` : ''
+            }}
+            id='shop'
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            to="/shop">SHOP</Link>
+          <Link className={`${hovered.id === 'plus' ? 'hovered' : ''} disabled`}
+            style={{
+              backgroundColor: hovered.id === 'plus' ? hovered.color : '',
+              outline: hovered.id === 'plus' ? `1px solid ${hovered.color}` : ''
+            }}
+            id='plus'
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            to="/plus">PLUS PLUS PLUS</Link>
+        </div>
+      </section >
       {/* <div style={{ height: 10 }}>
         hello
       </div> */}
-      <Joy />
+      <section className='joy'>
+        <Joy />
+      </section>
     </div>
   )
 }
