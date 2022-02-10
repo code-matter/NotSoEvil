ef96eefb40b369e8a235b4d3c815acd13af0aa57
Alex Caissy <alexandre@04h11.com>
2022-02-10 11:59:32 -0500

    [FEATURE] - added translation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import EvilHeader from '../components/EvilHeader'
 import Joy from '../components/Joy'
+import fraise from '../assets/fraise.png'
+import chien from '../assets/chien.png'
+import lapinvert from '../assets/lapinvert.png'
+import lapinjaune from '../assets/lapinjaune.png'
+import lapinbleu from '../assets/lapinbleu.png'
+import { useTranslation } from 'react-i18next'
+import i18next from 'i18next'
 
 interface IHovered {
   id: string,
@@ -10,9 +17,13 @@ interface IHovered {
 
 export const RANDOM_COLORS = ['#E1C3FF', '#15B7FF', '#2DE4D1', '#FACD01']
 
+export const toggleLocale = () => {
+  i18next.changeLanguage(i18next.language === "fr" ? "en" : "fr");
+};
+
 const HomePage: React.FC = () => {
   const isMobile = window.innerWidth < 500;
-
+  const { t } = useTranslation();
   const [hovered, setHovered] = useState<IHovered>({ id: '', color: '' })
 
   const handleMouseEnter = (event: any) => { // Need to fix TYPES
@@ -25,8 +36,10 @@ const HomePage: React.FC = () => {
     setHovered({ id: '', color: '' })
   }
 
+
   return (
     <div className='homepage container'>
+      <button style={{ position: 'absolute', zIndex: 999, top: 0, left: 0 }} onClick={toggleLocale}>{t('general.language')}</button>
       <section className='top'>
         <EvilHeader />
         <div className='text'>
@@ -43,7 +56,7 @@ const HomePage: React.FC = () => {
             id='rdv'
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            to="/form">RENDEZ-VOUS</Link>
+            to="/form">{t('general.rdv')}</Link>
           <Link className={`${hovered.id === 'flash' ? 'hovered' : ''} disabled`}
             style={{
               backgroundColor: hovered.id === 'flash' ? hovered.color : '',
@@ -61,7 +74,7 @@ const HomePage: React.FC = () => {
             id='shop'
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            to="/shop">SHOP</Link>
+            to="/shop">{t('general.shop')}</Link>
           <Link className={`${hovered.id === 'plus' ? 'hovered' : ''} disabled`}
             style={{
               backgroundColor: hovered.id === 'plus' ? hovered.color : '',
@@ -70,15 +83,25 @@ const HomePage: React.FC = () => {
             id='plus'
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            to="/plus">PLUS PLUS PLUS</Link>
+            to="/plus">{t('general.moremoremore')}</Link>
         </div>
       </section >
-      {/* <div style={{ height: 10 }}>
-        hello
-      </div> */}
-      <section className='joy'>
+      {/* <section className='joy'>
         <Joy />
-      </section>
+      </section> */}
+      {/* <section className='rabbits-photos'>
+        <div className='photos-container'>
+          <img className='top-left' src={fraise} alt="" />
+          <div className='rabbits-tr'>
+            <img className='top-right-b' src={lapinbleu} alt="" />
+            <img className='top-right-g' src={lapinvert} alt="" />
+          </div>
+        </div>
+        <div className='photos-container'>
+          <img className='bottom-left' src={lapinjaune} alt="" />
+          <img className='bottom-right' src={chien} alt="" />
+        </div>
+      </section> */}
     </div>
   )
 }
