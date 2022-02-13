61472dadff0b709c1a9ab066a323eaa2f76fa6ab
Alex Caissy <alexandre@04h11.com>
2022-02-13 09:42:42 -0500

    [FEATURE] - fixed language

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,26 @@ import {
 import HomePage from './pages/HomePage';
 import RDV from './pages/RDV';
 import i18next from 'i18next'
+import { useState } from 'react';
 
 function App() {
+  const [lang, setLang] = useState('fr')
   return (
     <div className="App">
       <div className='language-container'>
-        <p className={i18next.language === "fr" ? 'active' : 'inactive'}
-          onClick={() => i18next.changeLanguage('fr')}>FR</p>
+        <p className={lang === "fr" ? 'active' : 'inactive'}
+          onClick={() => {
+            i18next.changeLanguage('fr')
+            console.log(i18next.language)
+            setLang(i18next.language)
+          }}>FR</p>
         <span></span>
-        <p className={i18next.language === "en" ? 'active' : 'inactive'}
-          onClick={() => i18next.changeLanguage('en')}>EN</p>
+        <p className={lang === "en" ? 'active' : 'inactive'}
+          onClick={() => {
+            i18next.changeLanguage('en')
+            console.log(i18next.language)
+            setLang(i18next.language)
+          }}>EN</p>
       </div>
 
       <Routes>
