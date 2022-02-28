1f6ed679bea068866672dc715e7059087c141458
Alex Caissy <alexandre@04h11.com>
2022-02-28 14:37:42 -0500

    [FEATURE] - Edited my storybook

--- src/stories/CustomInput.stories.tsx
+++ src/stories/CustomInput.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import CustomInput from '../components/CustomInput';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/CustomInput',
+  title: 'Components/CustomInput',
   component: CustomInput,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
@@ -67,9 +67,9 @@ const Template: ComponentStory<typeof CustomInput> = (args) => {
   )
 };
 
-export const Main = Template.bind({});
+export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// Main.args = {
+// Default.args = {
 //   id: '532e381d-294e-4d93-b9b4-8781d8fcd9a7',
 //   label: 'Custom Label',
 // };
