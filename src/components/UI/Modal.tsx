0667e78b5b0a3cfc28098808d9146077acea9632
Alex Caissy <alexandre@04h11.com>
2022-03-05 15:15:34 -0500

    [FEATURE] - fixed the modal to center

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -4,20 +4,21 @@ import { RANDOM_COLORS } from "../../pages/HomePage";
 import { motion } from "framer-motion"
 
 const Backdrop = (props: any) => {
-  return <div className="modal-backdrop" onClick={props.onClose}></div>;
+  return <div className="modal-backdrop" onClick={props.onClose}>{props.children}</div>;
 };
 
 const ModalOverlay = (props: any) => {
   return (
     <motion.div className="modal"
-      initial={{ y: '100vh', x: '-50%' }}
-      animate={{ y: '-25vh', x: '-50%' }}
+      initial={{ y: 250, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
       transition={{
         type: "spring",
         damping: 50,
         mass: 3,
         stiffness: 500,
       }}
+    // style={{ backgroundColor: RANDOM_COLORS[Math.floor(Math.random() * RANDOM_COLORS.length)] + 'af' }}
     >
       <div className="modal-content">{props.children}</div>
     </motion.div>
@@ -28,13 +29,15 @@ const portalEl = document.getElementById("overlays");
 
 const Modal = (props: any) => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      {ReactDOM.createPortal(<Backdrop onClose={props.backdropClose ? props.onClose : null} />, portalEl as HTMLElement)}
+    <>
       {ReactDOM.createPortal(
-        <ModalOverlay>{props.children}</ModalOverlay>,
-        portalEl as HTMLElement
-      )}
-    </div>
+        <Backdrop
+          onClose={props.backdropClose ?
+            props.onClose :
+            null} >
+          <ModalOverlay>{props.children}</ModalOverlay>
+        </Backdrop>, portalEl as HTMLElement)}
+    </>
   );
 };
 
