9581f6211d756147333422d21ca330127121d960
Alex Caissy <alexandre@04h11.com>
2022-04-06 08:27:11 -0400

    [FEATURE] - added address

--- src/pages/OrderCompleted.tsx
+++ src/pages/OrderCompleted.tsx
@@ -10,98 +10,109 @@ export interface IOrderCompleted {
 const OrderCompleted = ({ }: IOrderCompleted) => {
   const { id } = useParams()
   // const { details, items } = location.state
-  const [items, setItems] = useState<any>()
+  const [items, setItems] = useState<any>([])
   const [details, setDetails] = useState<any>({})
 
   const fetchOrder = async () => {
     if (id) {
       const order: any = await OrdersService.get(id)
       if (order && order.orderDetails) {
+        console.log('details', order.orderDetails.details)
         setItems(order.orderDetails.items)
         setDetails(order.orderDetails.details)
       }
     }
   }
+
   useEffect(() => {
     fetchOrder()
+    return () => {
+      setItems([])
+      setDetails({})
+    }
+  }, [])
 
-    // return () => {
-    //   second
-    // }
-  }, [id])
-
+  useEffect(() => {
+    console.log('detailssssss', details)
+  }, [details])
 
   return (
     <div className="shop container">
-      {items && details && <div className="shop-order-summary">
-        <div className="left">
-          <p>Order Summary</p>
-          <h1>{details.id}</h1>
-          <div className="order-items">
-            {items.map((item: any, itemIdx: any) => {
-              return (
-                <div className="shop-order-view" key={item.id + itemIdx}>
-                  <img src={item.image} alt={item.image} />
-                  <div className="shop-order-infos">
-                    <div className="infos-title">
-                      <h1>{item.id}</h1>
-                      <h1>{item.price} $</h1>
-                    </div>
-                    <div className="infos-title description">
-                      <p>{item.rarity.toUpperCase()}</p>
-                      <p>{item.type}</p>
-                      <p>{item.size}</p>
+      {items.length && details && details.purchase_units &&
+        <div className="shop-order-summary">
+          <div className="left">
+            <p>Order Summary</p>
+            <h1>{details.id}</h1>
+            <div className="order-items">
+              {items.map((item: any, itemIdx: any) => {
+                return (
+                  <div className="shop-order-view" key={item.id + itemIdx}>
+                    <img src={item.image} alt={item.image} />
+                    <div className="shop-order-infos">
+                      <div className="infos-title">
+                        <h1>{item.id}</h1>
+                        <h1>{item.price} $</h1>
+                      </div>
+                      <div className="infos-title description">
+                        <p>{item.rarity.toUpperCase()}</p>
+                        <p>{item.type}</p>
+                        <p>{item.size}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
           </div>
-        </div>
-        <div className="right">
-          <p>Details</p>
-          <div className="total-info">
-            <h1>Total</h1>
-            <div className="order-items">
-              <div className="infos-title">
-                <h2>Subtotal</h2>
-                <h2>{items.length > 1 ?
-                  items.reduce(
-                    (prev: any, next: any) => prev + Number(next.price), 0).toFixed(2) :
-                  Number(items[0].price).toFixed(2)} $
-                </h2>
+          <div className="right">
+            <p>Details</p>
+            <div className="total-info">
+              <h1>Total</h1>
+              <div className="order-items">
+                <div className="infos-title">
+                  <h2>Subtotal</h2>
+                  <h2>{items.length > 1 ?
+                    items.reduce(
+                      (prev: any, next: any) => prev + Number(next.price), 0).toFixed(2) :
+                    Number(items[0].price).toFixed(2)} $
+                  </h2>
+                </div>
+                <div className="infos-title">
+                  <h2>Taxes</h2>
+                  <h2>{items.length > 1 ?
+                    (items.reduce(
+                      (prev: any, next: any) =>
+                        prev + Number(next.price), 0) * 0.15).toFixed(2) :
+                    (Number(items[0].price) * 0.15).toFixed(2)} $
+                  </h2>
+                </div>
+                <div className="infos-title">
+                  <h2>Shipping</h2>
+                  <h2>5.00 $</h2>
+                </div>
+                <div className="infos-title">
+                  <h2>Total</h2>
+                  <h2>{items.length > 1 ?
+                    (5 + items.reduce(
+                      (prev: any, next: any) =>
+                        prev + Number(next.price), 0) + items.reduce(
+                          (prev: any, next: any) =>
+                            prev + Number(next.price), 0) * 0.15).toFixed(2) :
+                    (5 + (Number(items[0].price)) * 1.15).toFixed(2)} $
+                  </h2>
+                </div>
               </div>
-              <div className="infos-title">
-                <h2>Taxes</h2>
-                <h2>{items.length > 1 ?
-                  (items.reduce(
-                    (prev: any, next: any) =>
-                      prev + Number(next.price), 0) * 0.15).toFixed(2) :
-                  (Number(items[0].price) * 0.15).toFixed(2)} $
-                </h2>
-              </div>
-              <div className="infos-title">
-                <h2>Shipping</h2>
-                <h2>5.00 $</h2>
-              </div>
-              <div className="infos-title">
-                <h2>Total</h2>
-                <h2>{items.length > 1 ?
-                  (5 + items.reduce(
-                    (prev: any, next: any) =>
-                      prev + Number(next.price), 0) + items.reduce(
-                        (prev: any, next: any) =>
-                          prev + Number(next.price), 0) * 0.15).toFixed(2) :
-                  (5 + (Number(items[0].price)) * 1.15).toFixed(2)} $
-                </h2>
+            </div>
+            <div className="adress-info">
+              <h1>Livré à:</h1>
+              <div className="adress">
+                <h4>{details.purchase_units[0].shipping.name.full_name}</h4>
+                <h4>{details.purchase_units[0].shipping.address.address_line_1}, {details.purchase_units[0].shipping.address.postal_code}</h4>
+                <h4>{details.purchase_units[0].shipping.address.admin_area_2}, {details.purchase_units[0].shipping.address.admin_area_1}, {details.purchase_units[0].shipping.address.country_code}</h4>
               </div>
             </div>
           </div>
-          <div className="adress-info">
-            ADRESS STUFF HERE
-          </div>
-        </div>
-      </div>}
+        </div>}
     </div >
   )
 }
